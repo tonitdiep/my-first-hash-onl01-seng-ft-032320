560e1bebf6c345f7f1cd57339937a5ead5ae284d
Toni  Diep <adiep123@gmail.com>
2020-04-04 22:27:41 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
- shipping_manifest["oil paintings"] = 3
+                shipping_manifest["oil paintings"] = 3
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
