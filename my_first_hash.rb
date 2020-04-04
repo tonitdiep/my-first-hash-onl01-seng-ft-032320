7aa5c7b7185b07d7adb294967b616ecd8ec65705
Toni  Diep <adiep123@gmail.com>
2020-04-04 14:00:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -23,12 +23,12 @@ def retrieval
 end
 
 def adding
-  shipping_manifest = {
+  shipping_manifest = ({
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3,
                 "muskets" => 2,
-              "gun powder" => 4}
+              "gun powder" => 4})
             end
   # add 2 muskets to the shipping_manifest hash below
 
