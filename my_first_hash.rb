c08e3c6b058dc756abf7cc475689a911aac9fba4
Toni  Diep <adiep123@gmail.com>
2020-04-04 22:23:31 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -28,9 +28,7 @@ def adding
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
                   "oil paintings" => 3,
-              #  "muskets" => 2,
-            #  {}"gun powder" => 4
-            }
+                            }
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 end
