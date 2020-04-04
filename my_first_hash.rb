a00ef453bdcb04b37d2992b88e895d748b2af896
Toni  Diep <adiep123@gmail.com>
2020-04-04 22:27:25 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,7 +18,7 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-["oil paintings"] = 3
+ shipping_manifest["oil paintings"] = 3
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 
 end
@@ -27,8 +27,7 @@ def adding
   shipping_manifest = {
                   "whale bone corsets" => 5,
                   "porcelain vases" => 2,
-                  "oil paintings" => 3,
-                            }
+                  "oil paintings" => 3,            }
 shipping_manifest["muskets"] = 2
 shipping_manifest["gun powder"] = 4
 end
